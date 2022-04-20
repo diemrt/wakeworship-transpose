49dae76a99899d136f0d3f67d7fd8a02c4df393d
diego <diego.martignoni@abenergie.it>
2022-04-20 18:01:50 +0200

    Adding search major key

---
+++ src/data/ChordProgressions.js
@@ -0,0 +1,9 @@
+export const MajorChordProgressions = [
+    ["A","Bm","C#m","D","E","F#","G#"],
+    ["B","c#m","D#m","E","F#","G#","A#"],
+    ["C","Dm","Em","F","G","A","B"],
+    ["D","Em","F#m","G","A","B","C#"],
+    ["E","F#m","G#m","A","B","C#","D#"],
+    ["F","Gm","Am","Bb","C","D","E"],
+    ["G","Am","Bm","C","D","E","F#"]
+];
